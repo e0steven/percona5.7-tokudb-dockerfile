a4c8c67b701cc1f861593e4c93a49d7b62f0ca03
e0steven <e0steven@gmail.com>
2019-01-04 11:11:25 -0500

    Update ps-entry.sh

--- ps-entry.sh
+++ ps-entry.sh
@@ -9,7 +9,7 @@ fi
 
     # Get config
     DATADIR="$("mysqld" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
-    if [ ! -e "$DATADIR/init.ok" ]; then
+    if [ ! -d "$DATADIR/mysql" ]; then
         if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
                         echo >&2 'error: database is uninitialized and password option is not specified '
                         echo >&2 '  You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
