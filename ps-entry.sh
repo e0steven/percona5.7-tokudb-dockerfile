5bd3c1b33b06d5288735edeacede93a76ba3eb4b
e0steven <e0steven@gmail.com>
2021-06-11 12:02:31 -0400

    Update ps-entry.sh

--- ps-entry.sh
+++ ps-entry.sh
@@ -12,15 +12,8 @@ fi
     DATADIR="$("mysqld" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
     echo $DATADIR
     echo "lets go...linking logs"
-    sudo ln -sf /dev/stderr /var/log/mysqld.log
-    chown mysql:mysql /var/log/mysqld.log
     echo "linked"
 
-    
-    if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]; then
-       mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql -p$MYSQL_ROOT_PASSWORD
-    fi
-
 echo "ensure tokudb..."
-ps-admin --docker --enable-tokudb -u root -p$MYSQL_ROOT_PASSWORD
+
 tail -f /var/log/mysqld.log
